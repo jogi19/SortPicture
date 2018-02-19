d0b453bf3d427b12ebbb9c14b861cf50d7b92cdb
Jochen Weiss <36038341+jogi19@users.noreply.github.com>
2018-02-19 11:43:01 +0100

    search for time and date

--- poc/get_exif_infos.py
+++ poc/get_exif_infos.py
@@ -1,11 +1,16 @@
-
-from __future__ import print_function
-
 import exifread
-f = open('DSCF8811.JPG')
+f = open('2013-06-21 09.24.32.jpg')
 
 tags = exifread.process_file(f)
 
 for tag in tags.keys():
     if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
-        print("Key: %s , value %s" % (tag, tags[tag]))
+        # print("Key: %s , value %s" % (tag, tags[tag]))
+        if "DateTimeDigitized" in tag:
+            print('#####################################')
+            print("Key: %s , value %s" % (tag, tags[tag]))
+            print('#####################################')
+        if "Image DateTime" in tag:
+            print('#####################################')
+            print("Key: %s , value %s" % (tag, tags[tag]))
+            print('#####################################')
